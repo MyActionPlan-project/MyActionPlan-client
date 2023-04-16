d35142ed09eefa24cf835059837174d67123338c
Pim Blom <pim.blom@icloud.com>
2023-04-16 15:37:13 +0200

    small update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import "./Footer.css"
 function Footer(){
 
     return(
-        <div>
+        <div className="footer" >
             
-            <p>"Need a nudge to get off the couch? Our to-do list site will guilt-trip you into productivity, because who needs Netflix when you can conquer the world, one task at a time!"</p>
-            <p>Application created in 2023 by SotDok, 8lom </p>        
+            <p className="footer">"Need a nudge to get off the couch? Our to-do list site will guilt-trip you into productivity, because who needs Netflix when you can conquer the world, one task at a time!"</p>
+            <p className="footer">Application created in 2023 by SotDok, 8lom </p>        
         </div> 
     )
 }
