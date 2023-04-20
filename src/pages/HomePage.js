0b443567889898ec80b000a4b7a1bb9383c4e797
Pim Blom <pim.blom@icloud.com>
2023-04-20 12:07:28 +0200

    add styling to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,60 +1,51 @@
-import './HomePage.css';
+// import './HomePage.css';
 import { Image } from 'react-bootstrap';
 import {Container, Row, Col} from  'react-bootstrap'
 
 
 function HomePage() {
   return (
-    <Container fluid>
+    <Container className="bg-#A6BB8D" fluid>
     <Row>
-      <Col sm={12} md={8} className="mt-5 d-flex">
-        <h1 className="homeText">
-          {" "}
+      <Col sm={12} md={6} className="mt-5 d-flex">
+        <h1 className="display-4 fw-bolder ms-5 text-white"  >
+          
           From planning to productivity, Our app keeps you one step ahead!
         </h1>
       </Col>
     
-       <Col sm={6} md={4}  >
-          <Image
-            className="homeImg"
-            src="/preview.png"
-            alt="homepage"
-            fluid
-          />
-        </Col>
+      <Col sm={4} md={4}>
+          <Image  className="mt-5 shadow rounded" src="/preview.png" alt="homepage" fluid />
+          </Col>
       </Row>
 
-      <Row className=" mt-5 d-flex">
+      <Row className="font-size-small mt-5 d-flex text-white ">
 
-        <Col className="d-flex flex-direction-column justify-content-center">
-          <Col sm={4} className="d-flex flex-direction-column justify-content-center">
-            <img  src="/icons/check-all.svg" alt="check-all" width="23%" height="23%"></img>
-            <p className="max-width ">
-              {" "}
+          <Col sm={12} md={4} className="d-flex flex-wrap-wrap ">
+            <img className="me-4" src="/icons/check-all.svg" alt="check-all" width="65" height="65"></img>
+            <p className="fs-5 ">
               Focus on what matters: With our simple and intuitive interface,
               you can quickly and easily add only the necessary tasks to your
               to-do list.
             </p>
           </Col>
 
-          <Col sm={4} className="d-flex flex-direction-column justify-content-center">
-          <img  src="/icons/globe2.svg" alt="globe" width="20%" height="20%" ></img>
+          <Col sm={12} md={4} className="d-flex  ">
+          <img className="me-4" src="/icons/globe2.svg" alt="globe" width="65" height="65" ></img>
 
-            <p className="max-width">
-              {" "}
+            <p className="fs-5">
               Accessible anywhere, anytime: Our app is cloud-based, so you can
               access it from anywhere and on any device. Whether you're at home,
               at work, or on the go, our app is always there when you need it.
             </p>
           </Col>
-          <Col sm={4} className="d-flex flex-direction-column " >
-          <img  src="/icons/graph-up-arrow.svg" alt="graph-up-arrow" width="20%" height="20%"></img>
-          <p className="max-width">
-          {" "}
+          <Col sm={12} md={4} className="d-flex " >
+          <img className="me-4" src="/icons/graph-up-arrow.svg" alt="graph-up-arrow" width="65" height="65"></img>
+          <p className="fs-5">
           Boost your productivity: Our to-do app is designed to help you get more done in less time. 
           </p>
           </Col>
-        </Col>
+    
        
       </Row>
     </Container>
